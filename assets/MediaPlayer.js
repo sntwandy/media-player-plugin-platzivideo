416b3242855831b2a780c2a35f45d4ddee68a531
sntwandy <sntwandy@gmail.com>
2020-06-06 11:40:23 -0400

    Crating mute and unmute btn and their functionality

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,15 +1,30 @@
-class MediaPlayer {
-    constructor(config) {
-        this.media = config;
-    }
+function MediaPlayer (config) {
+    this.media = config.el;
+    this.plugins = config.plugin || [];
 
-    play(){
-        this.media.play();
-    }
+    this._initPlugins();
+};
 
-    pause(){
-        this.media.pause();
-    }
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
+
+MediaPlayer.prototype.play = function () {
+    this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+    this.media.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted = false;
 }
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,16 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+const play_pause_btn = document.getElementById("play-pause-btn");
+const mute_unmute_btn = document.getElementById("mute-unmute-btn");
 
-const media = new MediaPlayer(video);
+const media = new MediaPlayer({ el: video  ,plugin: [new AutoPlay()] });
 
-button.onclick = () => {
+play_pause_btn.onclick = () => {
     (video.paused) ? media.play() : media.pause();
+}
+
+mute_unmute_btn.onclick = () => {
+    (video.muted) ? media.unmute() : media.mute();
 }
