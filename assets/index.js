0e4a96852211389d5b55aad497de6da1a70eb22b
sntwandy <sntwandy@gmail.com>
2020-06-09 00:15:36 -0400

    Adding a new plugin, AutoPause

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector("video");
 const play_pause_btn = document.getElementById("play-pause-btn");
 const mute_unmute_btn = document.getElementById("mute-unmute-btn");
 
-const media = new MediaPlayer({ el: video  ,plugins: [new AutoPlay()] });
+const media = new MediaPlayer({ el: video  ,plugins: [new AutoPlay(), new AutoPause()]});
 
 play_pause_btn.onclick = () => {
     (video.paused) ? media.play() : media.pause();
