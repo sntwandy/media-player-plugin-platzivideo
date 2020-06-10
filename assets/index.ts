14c083ba8e8d23799838c29fbb786d8cdf6c0e8e
sntwandy <sntwandy@gmail.com>
2020-06-09 23:22:08 -0400

    Adding an Ads plugin

--- assets/index.ts
+++ assets/index.ts
@@ -1,12 +1,13 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector("video");
 const play_pause_btn: HTMLElement = document.getElementById("play-pause-btn");
 const mute_unmute_btn: HTMLElement = document.getElementById("mute-unmute-btn");
 
-const media = new MediaPlayer({ el: video  ,plugins: [new AutoPlay(), new AutoPause()]});
+const media = new MediaPlayer({ el: video  ,plugins: [new AutoPlay(), new AutoPause(), new Ads()]});
 
 play_pause_btn.onclick = () => {
     (video.paused) ? media.play() : media.pause();
